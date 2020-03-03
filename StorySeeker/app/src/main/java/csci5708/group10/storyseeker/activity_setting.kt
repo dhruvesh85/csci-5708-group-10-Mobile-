74c4e8f6dfb262d2bbee6e01cbdf9d43fb6d502a
Vali Shaik <vl216084@dal.ca>
2020-03-03 00:15:35 -0400

    UI designs for profile,settings,buy

---
+++ StorySeeker/app/src/main/java/csci5708/group10/storyseeker/activity_setting.kt
@@ -0,0 +1,12 @@
+package csci5708.group10.storyseeker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_setting : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
---
+++ StorySeeker/app/src/main/java/csci5708/group10/storyseeker/profile.kt
@@ -0,0 +1,12 @@
+package csci5708.group10.storyseeker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
